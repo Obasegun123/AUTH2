7003eb730ed203827192174ac7c4844200266f0f
richardnwonah <nwonahr@gmail.com>
2023-07-24 20:30:57 +0100

    added implementation for user names and profile photo

---
+++ IdentityDemo/Areas/Identity/Data/IdentityDemoContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityDemo.Data;
+
+public class IdentityDemoContext : IdentityDbContext<ApplicationUser>
+{
+    public IdentityDemoContext(DbContextOptions<IdentityDemoContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+
+        base.OnModelCreating(builder);
+    }
+}
